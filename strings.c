e42c2b909d4d33985ca4838e20d071e6757bc111
Kyle Andrews <codingwithkyle@gmail.com>
2024-02-02 10:34:23 -0500

    😐 More strings

--- strings.c
+++ strings.c
@@ -6,7 +6,11 @@ int main()
 {
     int ids[MAX_IDS] = {0,1,2};
 
-    char mystr[] = {'h','e','l','l','o'};
+    char mystr[] = {'h','e','l','l','o', 0}; // 0 is the null terminator
+    char *myotherstr = "world"; // null terminator is automatically added
 
     printf("%s\n", mystr);
+
+    //strcpy(destPtr, srcPtr);
+    //strncpy(destPtr, srcPtr, n);
 }
