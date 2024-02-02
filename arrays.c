32f8bc3958d7866acde2afa184a981937cea458f
Kyle Andrews <codingwithkyle@gmail.com>
2024-02-02 10:31:25 -0500

    🧠 Arrays and strings

---
+++ arrays.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+#define MAX_IDS 32
+
+int main()
+{
+    int ids[MAX_IDS] = {0,1,2};
+
+    printf("%d\n", ids[0]);
+
+    ids[3] = 0x41;
+
+    printf("%d\n", ids[3]);
+}
---
+++ strings.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+#define MAX_IDS 32
+
+int main()
+{
+    int ids[MAX_IDS] = {0,1,2};
+
+    char mystr[] = {'h','e','l','l','o'};
+
+    printf("%s\n", mystr);
+}
