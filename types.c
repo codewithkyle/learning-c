1ccbc53e3e21004a73b95fce18d9b212459e13af
Kyle Andrews <codingwithkyle@gmail.com>
2024-02-03 10:02:05 -0500

    😐 type casting

--- types.c
+++ types.c
@@ -9,4 +9,23 @@ int main(int argc, char *argv[])
 
     float f; // low accuracy
     double d; // higher accuracy
+
+    // same size casting
+    unsigned int x = 4;
+    int sx = (int)(x); // type casting
+
+    // down casting
+    unsigned int bigx = 0xfffffefe;
+    short kindabig = (short)bigx;
+
+    // up casting
+    short short2 = -1; // 0xffff
+    int wasashrot = (int)short2; // 0xffffffff
+
+    // cross casting
+    float float2 = 3.14;
+    int wasafloat = (int)float2;
+
+    // pointer casting
+    short *mydata = (short*)argv[1]; // casts char pointer to short pointer
 }
