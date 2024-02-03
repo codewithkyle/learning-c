8b73fc8c759fd9973dac278fd6a7f37482ed7290
Kyle Andrews <codingwithkyle@gmail.com>
2024-02-03 09:55:35 -0500

    🧠 types

---
+++ types.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+    char y; // 1 byte, 8 bits
+    short s; // 2 bytes, 16 bits
+    int i; // 4 bytes, 32 bits
+    long long int l; // 8 bytes, 64 bit
+
+    float f; // low accuracy
+    double d; // higher accuracy
+}
